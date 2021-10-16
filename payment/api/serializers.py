92d19de38f529cd6add5d4c3a83168011223f086
tejas-trivedi <500077185@stu.upes.ac.in>
2021-10-16 14:22:22 +0530

    order generated

---
+++ payment/api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from payment.models import Order
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_date = serializers.DateTimeField(format="%d %B %Y %I:%M %p")
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+        depth = 2
